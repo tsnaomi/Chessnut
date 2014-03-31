3b7813db55471c13c7a100008b9083219086d231
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-31 15:55:42 -0700

    Added skeleton tests for pawn evaluation logic.

--- chessnut/test_chess_evaluators.py
+++ chessnut/test_chess_evaluators.py
@@ -3,7 +3,27 @@ from chess import ChessnutGame
 
 
 class TestPawnEvaluator(unittest.TestCase):
-    pass
+    """Test the pawn logic evaluator."""
+    def setUp(self):
+        self.c = ChessnutGame()
+
+    def move_pawn_forward_1(self):
+        pass
+
+    def move_pawn_forward_2_from_start(self):
+        pass
+
+    def move_pawn_diagonally_not_capture(self):
+        pass
+
+    def move_pawn_forward_2_not_start(self):
+        pass
+
+    def move_pawn_backward(self):
+        pass
+
+    def move_pawn_sideways(self):
+        pass
 
 
 if __name__ == '__main__':
