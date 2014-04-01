2653fa743c61fddf4805febc250284c8badd1cf3
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-01 09:20:39 -0700

    Added skeleton tests for king evaluator.

--- chessnut/test_king_evaluator.py
+++ chessnut/test_king_evaluator.py
@@ -16,6 +16,46 @@ class TestKingEvaluator(unittest.TestCase):
             'checkmate': None,
         }
 
+    def test_move_king_horizontally(self):
+        """Move a king horizontally and assert that this move is
+        determined legal.
+        """
+
+    def test_move_king_diagonally(self):
+        """Move a king diagonally and assert that this move is determined
+        legal.
+        """
+
+    def test_move_king_horizontally_multiple_spaces(self):
+        """Move a king horizontally more than one space and assert that
+        this move is determined illegal.
+        """
+
+    def test_move_king_diagonally_multiple_spaces(self):
+        """Move a king diagonally more than one space and assert that
+        this move is determined illegal.
+        """
+
+    def test_move_king_horizontally_blocked(self):
+        """Move a king horizontally when it is blocked and assert that
+        this move is determined illegal.
+        """
+
+    def test_move_king_diagonally_blocked(self):
+        """Move a king diagonally when it is blocked and assert that
+        this move is determined illegal.
+        """
+
+    def test_capture_king_horizontally(self):
+        """Capture pieces that lie horizontally around a king and assert
+        that this move is determined legal.
+        """
+
+    def test_capture_king_vertically(self):
+        """Capture pieces that lie diagonally around a king and assert
+        that this move is determined legal.
+        """
+
 
 if __name__ == '__main__':
     unittest.main()
