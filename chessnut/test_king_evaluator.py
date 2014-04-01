d18bcc96596fb587618314c244bd9c0b34606f98
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-01 09:47:48 -0700

    Added movement and capturing logic tests for king evaluator.

--- chessnut/test_king_evaluator.py
+++ chessnut/test_king_evaluator.py
@@ -20,41 +20,111 @@ class TestKingEvaluator(unittest.TestCase):
         """Move a king horizontally and assert that this move is
         determined legal.
         """
+        for king in [('K', True), ('K', False)]:
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['e3', 'e5', 'd4', 'f4']:
+                self.groups['dest'] = dest
+                self.assertEqual(self.c._king_evaluator(self.groups), (4, 4))
 
     def test_move_king_diagonally(self):
         """Move a king diagonally and assert that this move is determined
         legal.
         """
+        for king in [('K', True), ('K', False)]:
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['d3', 'd5', 'f3', 'f5']:
+                self.groups['dest'] = dest
+                self.assertEqual(self.c._king_evaluator(self.groups), (4, 4))
 
     def test_move_king_horizontally_multiple_spaces(self):
         """Move a king horizontally more than one space and assert that
         this move is determined illegal.
         """
+        self.c.board[1] = [(0, 0) for i in range(8)]
+        self.c.board[6] = [(0, 0) for i in range(8)]
+        for king in [('K', True), ('K', False)]:
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['e2', 'e6', 'c4', 'g4']:
+                self.groups['dest'] = dest
+                self.assertRaises(
+                    MoveNotLegalError, self.c._king_evaluator, self.groups)
 
     def test_move_king_diagonally_multiple_spaces(self):
         """Move a king diagonally more than one space and assert that
         this move is determined illegal.
         """
+        self.c.board[1] = [(0, 0) for i in range(8)]
+        self.c.board[6] = [(0, 0) for i in range(8)]
+        for king in [('K', True), ('K', False)]:
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['c2', 'c6', 'g2', 'g6']:
+                self.groups['dest'] = dest
+                self.assertRaises(
+                    MoveNotLegalError, self.c._king_evaluator, self.groups)
 
     def test_move_king_horizontally_blocked(self):
         """Move a king horizontally when it is blocked and assert that
         this move is determined illegal.
         """
+        for king in [('K', True), ('K', False)]:
+            for i in [3, 4, 5]:
+                for j in [3, 4, 5]:
+                    self.c.board[i][j] = ('P', king[1])
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['e3', 'e5', 'd4', 'f4']:
+                self.groups['dest'] = dest
+                self.assertRaises(
+                    MoveNotLegalError, self.c._king_evaluator, self.groups)
 
     def test_move_king_diagonally_blocked(self):
         """Move a king diagonally when it is blocked and assert that
         this move is determined illegal.
         """
+        for king in [('K', True), ('K', False)]:
+            for i in [3, 4, 5]:
+                for j in [3, 4, 5]:
+                    self.c.board[i][j] = ('P', king[1])
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['d3', 'd5', 'f3', 'f5']:
+                self.groups['dest'] = dest
+                self.assertRaises(
+                    MoveNotLegalError, self.c._king_evaluator, self.groups)
 
     def test_capture_king_horizontally(self):
         """Capture pieces that lie horizontally around a king and assert
         that this move is determined legal.
         """
+        self.groups['capture'] = 'x'
+        for king in [('K', True), ('K', False)]:
+            for i in [3, 4, 5]:
+                for j in [3, 4, 5]:
+                    self.c.board[i][j] = ('P', not king[1])
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['e3', 'e5', 'd4', 'f4']:
+                self.groups['dest'] = dest
+                self.assertEqual(self.c._king_evaluator(self.groups), (4, 4))
 
     def test_capture_king_vertically(self):
         """Capture pieces that lie diagonally around a king and assert
         that this move is determined legal.
         """
+        self.groups['capture'] = 'x'
+        for king in [('K', True), ('K', False)]:
+            for i in [3, 4, 5]:
+                for j in [3, 4, 5]:
+                    self.c.board[i][j] = ('P', not king[1])
+            self.c.turn = king[1]
+            self.c.board[4][4] = king
+            for dest in ['d3', 'd5', 'f3', 'f5']:
+                self.groups['dest'] = dest
+                self.assertEqual(self.c._king_evaluator(self.groups), (4, 4))
 
 
 if __name__ == '__main__':
