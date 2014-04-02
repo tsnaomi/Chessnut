10944943f66dd77454c4f80cbdc03e020f0e0ea8
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-02 14:37:08 -0700

    Added syntax for starting a game.

--- chessnut/test_tweet_parser.py
+++ chessnut/test_tweet_parser.py
@@ -12,11 +12,24 @@ class TestTweetParser(unittest.TestCase):
             'move': 'Nb2c3+',
             'message': 'suck it bob',
         }
+        self.start_tweet = \
+            u'@ChessnutApp @bob #bobisgoingdown a3 bob I challenge you!'
+        self.start_expected = {
+            'game': 'bobisgoingdown',
+            'move': 'a3',
+            'message': 'bob I challenge you!',
+            'opponent': 'bob',
+        }
 
     def test_good_tweet(self):
         groups = tweet_parser(self.good_tweet)
         for key in self.expected:
             self.assertEqual(self.expected[key], groups[key])
+
+    def test_start_tweet(self):
+        groups = tweet_parser(self.start_tweet)
+        for key in self.start_expected:
+            self.assertEqual(self.start_expected[key], groups[key])
 
     def test_bad_tweet(self):
         self.assertRaises(ValueError, tweet_parser, self.bad_tweet)
