c00cc01b8b3c15f204a532cf415321939a9b02dd
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-02 14:29:24 -0700

    Added tweet parser and quick tests for tweet parser.

---
+++ chessnut/tweet_parser.py
@@ -0,0 +1,18 @@
+import re
+
+
+def tweet_parser(tweet):
+    """Takes in a unicode-formatted tweet and returns a dictionary of the
+    game name, owner, opponent, move, and extra message.
+    """
+    tweet = tweet.encode()
+
+    match = re.match(
+        r'^@\w{11} #(?P<game>\w+) (?P<move>[^\s]+) (?P<message>.*)$',
+        tweet
+    )
+
+    if match:
+        return match.groupdict()
+    else:
+        raise ValueError("Tweet not formatted correctly.")
