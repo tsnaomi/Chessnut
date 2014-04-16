0b7f50a1611f994b8f0585f2daeb85b10c0b85f4
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-16 13:50:55 -0700

    Added buckets for diagonal slices.

--- chessnut_game/game.py
+++ chessnut_game/game.py
@@ -14,10 +14,14 @@ class ChessnutGame(object):
         self.turn = White
 
         #Bucket the pieces on the board by several criteria.
+
+        #Bucket by the owner of the piece.
         self.pieces_by_player = {
             White: set(),
             Black: set(),
         }
+
+        #Bucket by the type of piece.
         self.pieces_by_type = {
             'pawn': set(),
             'rook': set(),
@@ -26,6 +30,8 @@ class ChessnutGame(object):
             'queen': set(),
             'king': set(),
         }
+
+        #Bucket by rank and by file.
         self.pieces_by_rank = {
             '1': set(),
             '2': set(),
@@ -45,6 +51,54 @@ class ChessnutGame(object):
             'f': set(),
             'g': set(),
             'h': set(),
+        }
+
+        #Bucket pieces by the diagonal on which they lie. Diagonals
+        #originate from the the left and/or bottom edges of the board.
+        #For example, a8 is the forward diagonal starting from space a8
+        #and extending up and to the right of a8 - where there are no spaces,
+        #meaning that this diagonal contains only one space. a8 is also
+        #the backward diagonal starting at space a8 and extending down
+        #and to the left of a8 - all the way across the board to space h1.
+
+        #Forward diagonal are from bottom left to top right, like a
+        #forward slash '/'
+        self.pieces_by_forward_diagonal = {
+            'a8': set(),
+            'a7': set(),
+            'a6': set(),
+            'a5': set(),
+            'a4': set(),
+            'a3': set(),
+            'a2': set(),
+            'a1': set(),
+            'b1': set(),
+            'c1': set(),
+            'd1': set(),
+            'e1': set(),
+            'f1': set(),
+            'g1': set(),
+            'h1': set(),
+        }
+
+        #Backward diagonal are from top left to bottom right, like a
+        #backslash '\'
+        self.pieces_by_backward_diagonal = {
+            'a8': set(),
+            'a7': set(),
+            'a6': set(),
+            'a5': set(),
+            'a4': set(),
+            'a3': set(),
+            'a2': set(),
+            'a1': set(),
+            'b1': set(),
+            'c1': set(),
+            'd1': set(),
+            'e1': set(),
+            'f1': set(),
+            'g1': set(),
+            'h1': set(),
         }
 
     def reconstruct_from_pgn():
