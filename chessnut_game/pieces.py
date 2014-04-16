084158dfa64f178777107d76f1064108c9a086e9
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-16 15:05:02 -0700

    Added sets of cached moves to the piece class.

--- chessnut_game/pieces.py
+++ chessnut_game/pieces.py
@@ -4,10 +4,37 @@ class Piece(object):
     legality of that move), and knows to which player it belongs.
     """
     def __init__(self, player=None, rank=None, _file=None):
+        #Keep track of whose piece this is and where it is on the board.
         self.player = player
         self.rank = rank
         self.file = _file
-        self.moves = set()
+
+        #Store a set containing all the spaces on the board to which this
+        #piece could move - IF this piece were the only piece on the board.
+        #This much, the piece can calculate on its own, because it knows
+        #its own move logic.
+        self.naive_moves = set()
+
+        #Store a set containing all of the spaces on the board to which
+        #this piece can ACTUALLY move, given the state of the board. This
+        #attribute is set by the game object, as the piece does not know
+        #about the positions of other pieces on its own.
+        self.actual_moves = set()
+
+        #Store a set containing all the spaces on the board to which this
+        #piece could capture - IF this piece were the only piece on the
+        #board, save for a single enemy piece at the space being captured
+        #to. The piece can calculate this on its own because it is familiar
+        #with its own move logic. For all pieces save the pawn, this set
+        #will be identical to the naive_moves set.
+        self.naive_captures = self.naive_moves
+
+        #Store a set containing all of the spaces on the board to which
+        #this piece can ACTUALLY capture. This attribute is set by the
+        #game object, as it knows about the state of the board, while the
+        #piece does not. For all pieces save the pawn, this set will be
+        #identical to the actual_moves set.
+        self.actual_captures = self.actual_moves
 
 
 class Pawn(Piece):
