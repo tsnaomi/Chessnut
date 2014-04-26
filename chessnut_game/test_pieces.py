b52e172ff8157eec685313c244c01e26f1b6d343
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-25 17:14:16 -0700

    Added empty test cases for pieces.

---
+++ chessnut_game/test_pieces.py
@@ -0,0 +1,43 @@
+import unittest
+from pieces import Piece, Pawn, Rook, QueensideRook, KingsideRook, \
+    Knight, Bishop, Queen, King
+
+
+class TestPiece(unittest.TestCase):
+    """Test the Piece class."""
+
+
+class TestPawn(unittest.TestCase):
+    """Test the Pawn class."""
+
+
+class TestRook(unittest.TestCase):
+    """Test the Rook class."""
+
+
+class TestQueensideRook(unittest.TestCase):
+    """Test the QueensideRook class."""
+
+
+class TestKingsideRook(unittest.TestCase):
+    """Test the KingsideRook class."""
+
+
+class TestKnight(unittest.TestCase):
+    """Test the Knight class."""
+
+
+class TestBishop(unittest.TestCase):
+    """Test the Bishop class."""
+
+
+class TestQueen(unittest.TestCase):
+    """Test the Queen class."""
+
+
+class TestKing(unittest.TestCase):
+    """Test the King class."""
+
+
+if __name__ == '__main__':
+    unittest.main()
