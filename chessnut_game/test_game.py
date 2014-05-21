48396ee67c47e55ea6137e121802967c4e5865c9
geekofalltrades <thegeekofalltrades@gmail.com>
2014-05-21 15:14:47 -0700

    Reworked tests for _coordinates_to_(x)_diagonal to reflect change in game class functionality.

--- chessnut_game/test_game.py
+++ chessnut_game/test_game.py
@@ -2,68 +2,59 @@ import unittest
 from game import ChessnutGame
 
 
-class TestSANToDiagonals(unittest.TestCase):
-    """Test the _san_to_forward_diagonal and _san_to_backward_diagonal
-    methods.
+class TestCoordinatesToDiagonals(unittest.TestCase):
+    """Test the _coordinates_to_forward_diagonal and
+    _coordinates_to_backward_diagonal methods.
     """
     def setUp(self):
         self.c = ChessnutGame()
 
-    def build_forward_diagonal(self, rank, _file):
+    def build_forward_diagonal(self, _file, rank):
         """Determine all the spaces in a forward diagonal beginning at
         the specified cell.
         """
-        diagonals = []
-        rankord = ord(rank)
-        fileord = ord(_file)
-        while rankord <= 56 and fileord <= 104:
-            diagonals.append((chr(rankord), chr(fileord)))
-            rankord += 1
-            fileord += 1
+        while 0 <= rank <= 7 and 0 <= _file <= 7:
+            yield (_file, rank)
+            _file += 1
+            rank += 1
 
-        return diagonals
-
-    def build_backward_diagonal(self, rank, _file):
+    def build_backward_diagonal(self, _file, rank):
         """Determine all the spaces in a backward diagonal beginning at
         the specified cell.
         """
-        diagonals = []
-        rankord = ord(rank)
-        fileord = ord(_file)
-        while rankord <= 56 and fileord >= 97:
-            diagonals.append((chr(rankord), chr(fileord)))
-            rankord += 1
-            fileord -= 1
+        while 0 <= rank <= 7 and 0 <= _file <= 7:
+            yield (_file, rank)
+            _file -= 1
+            rank += 1
 
-        return diagonals
-
-    def test_san_to_forward_diagonal(self):
+    def test_coordinates_to_forward_diagonal(self):
         """Assert that all spaces are assigned to their correct forward
         diagonal.
         """
         diagonals = [
-            ('a', '8'),
-            ('a', '7'),
-            ('a', '6'),
-            ('a', '5'),
-            ('a', '4'),
-            ('a', '3'),
-            ('a', '2'),
-            ('a', '1'),
-            ('b', '1'),
-            ('c', '1'),
-            ('d', '1'),
-            ('e', '1'),
-            ('f', '1'),
-            ('g', '1'),
-            ('h', '1'),
+            (0, 7),
+            (0, 6),
+            (0, 5),
+            (0, 4),
+            (0, 3),
+            (0, 2),
+            (0, 1),
+            (0, 0),
+            (1, 0),
+            (2, 0),
+            (3, 0),
+            (4, 0),
+            (5, 0),
+            (6, 0),
+            (7, 0),
         ]
 
         for _file, rank in diagonals:
-            expected = ''.join([_file, rank])
-            for dirank, difile in self.build_forward_diagonal(rank, _file):
+            for difile, dirank in self.build_forward_diagonal(_file, rank):
                 self.assertEqual(
-                    expected, self.c._san_to_forward_diagonal(dirank, difile))
+                    (_file, rank),
+                    self.c._coordinates_to_forward_diagonal(difile, dirank)
+                )
 
     def test_san_to_backward_diagonal(self):
         """Assert that all spaces are assigned to their correct backward
@@ -88,10 +79,11 @@ class TestSANToDiagonals(unittest.TestCase):
         ]
 
         for _file, rank in diagonals:
-            expected = ''.join([_file, rank])
-            for dirank, difile in self.build_backward_diagonal(rank, _file):
+            for difile, dirank in self.build_backward_diagonal(_file, rank):
                 self.assertEqual(
-                    expected, self.c._san_to_backward_diagonal(dirank, difile))
+                    (_file, rank),
+                    self.c._coordinates_to_backward_diagonal(difile, dirank)
+                )
 
 
 if __name__ == '__main__':
