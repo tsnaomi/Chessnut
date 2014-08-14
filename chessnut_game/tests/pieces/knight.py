fee819edc8476924b5c7000f570b4b9dc0c89f12
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-13 22:12:35 -0700

    Alter knight tests to make use of decorators.

--- chessnut_game/tests/pieces/knight.py
+++ chessnut_game/tests/pieces/knight.py
@@ -1,84 +1,104 @@
 import unittest
+from ..utils.decorators import all_spaces, all_players
 from itertools import combinations
-from chessnut_game.game import Black, White, ChessnutGame
+from chessnut_game import ChessnutGame
 from chessnut_game.pieces import Knight, Pawn
 
 
 class TestKnight(unittest.TestCase):
     """Test the Knight class."""
-    def setUp(self):
-        self.pW = Knight(White, 0, 0)
-        self.pB = Knight(Black, 0, 7)
+    knight = Knight()
+    knight_modifiers = (
+        (1, 2),
+        (1, -2),
+        (2, 1),
+        (2, -1),
+        (-1, 2),
+        (-1, -2),
+        (-2, 1),
+        (-2, -1),
+    )
 
-    def test_generate_naive_cache(self):
-        """Assert that the naive_moves cache is correctly generated."""
-        for _file, rank in ((f, r) for f in range(8) for r in range(8)):
-            for piece in (self.pW, self.pB):
-                piece.file = _file
-                piece.rank = rank
+    @all_spaces
+    @all_players
+    def test_generate_naive_cache(self, player=None, _file=None, rank=None):
+        """Assert that the naive_moves cache is correctly generated from
+        the given space."""
+        self.knight.player = player
+        self.knight.file = _file
+        self.knight.rank = rank
 
-                expected_spaces = set()
-                for filemod, rankmod in (
-                        (1, 2), (1, -2), (2, 1), (2, -1),
-                        (-1, 2), (-1, -2), (-2, 1), (-2, -1)):
-                    if 0 <= piece.file + filemod <= 7 and \
-                            0 <= piece.rank + rankmod <= 7:
-                        expected_spaces.add(
-                            (piece.file + filemod, piece.rank + rankmod)
-                        )
+        expected_spaces = set()
+        for filemod, rankmod in self.knight_modifiers:
+            if 0 <= self.knight.file + filemod <= 7 and \
+                    0 <= self.knight.rank + rankmod <= 7:
+                expected_spaces.add(
+                    (self.knight.file + filemod, self.knight.rank + rankmod)
+                )
 
-                piece.generate_naive_cache()
+        self.knight.generate_naive_cache()
 
-                self.assertEqual(piece.naive_moves, expected_spaces)
+        self.assertEqual(self.knight.naive_moves, expected_spaces)
 
-    def test_generate_actual_cache_blockers(self):
-        """Assert that the actual_moves cache is correctly generated even
-        when various naive_moves locations are blocked by friendly pieces.
+    @all_spaces
+    @all_players
+    def test_generate_actual_cache_blockers(
+        self,
+        player=None,
+        _file=None,
+        rank=None
+    ):
+        """Assert that the actual_moves cache is correctly generated from
+        the given space even when various naive_moves locations are blocked
+        by friendly pieces.
         """
-        for _file, rank in ((f, r) for f in range(8) for r in range(8)):
-            for piece in (self.pW, self.pB):
-                piece.file = _file
-                piece.rank = rank
-                piece.generate_naive_cache()
+        self.knight.player = player
+        self.knight.file = _file
+        self.knight.rank = rank
+        self.knight.generate_naive_cache()
 
-                for i in range(len(piece.naive_moves)):
-                    for spaces in combinations(piece.naive_moves, i):
-                        game = ChessnutGame()
-                        for _file, rank in spaces:
-                            blocking_piece = Pawn(
-                                piece.player,
-                                _file,
-                                rank,
-                            )
-                            game._hard_place_piece(blocking_piece)
+        for i in range(len(self.knight.naive_moves)):
+            for spaces in combinations(self.knight.naive_moves, i):
+                game = ChessnutGame()
+                for block_file, block_rank in spaces:
+                    game._hard_place_piece(
+                        Pawn(self.knight.player, block_file, block_rank)
+                    )
 
-                        piece.generate_actual_cache(game)
+                self.knight.generate_actual_cache(game)
 
-                        for space in piece.actual_moves:
-                            self.assertIn(space, piece.naive_moves)
-                            self.assertNotIn(space, spaces)
+                for space in self.knight.actual_moves:
+                    self.assertIn(space, self.knight.naive_moves)
+                    self.assertNotIn(space, spaces)
 
-    def test_generate_actual_cache_enemies(self):
-        """Assert that the actual_moves cache is correctly generated even
-        when various naive_moves locations are occupied by enemy pieces.
+    @all_spaces
+    @all_players
+    def test_generate_actual_cache_enemies(
+        self,
+        player=None,
+        _file=None,
+        rank=None
+    ):
+        """Assert that the actual_moves cache is correctly generated from
+        the given space even when various naive_moves locations are occupied
+        by enemy pieces.
         """
-        for _file, rank in ((f, r) for f in range(8) for r in range(8)):
-            for piece in (self.pW, self.pB):
-                piece.file = _file
-                piece.rank = rank
-                piece.generate_naive_cache()
+        self.knight.player = player
+        self.knight.file = _file
+        self.knight.rank = rank
+        self.knight.generate_naive_cache()
 
-                for i in range(len(piece.naive_moves)):
-                    for spaces in combinations(piece.naive_moves, i):
-                        game = ChessnutGame()
-                        for _file, rank in spaces:
-                            blocking_piece = Pawn(
-                                not piece.player,
-                                _file,
-                                rank,
-                            )
-                            game._hard_place_piece(blocking_piece)
+        for i in range(len(self.knight.naive_moves)):
+            for spaces in combinations(self.knight.naive_moves, i):
+                game = ChessnutGame()
+                for block_file, block_rank in spaces:
+                    game._hard_place_piece(
+                        Pawn(not self.knight.player, block_file, block_rank)
+                    )
 
-                        piece.generate_actual_cache(game)
+                self.knight.generate_actual_cache(game)
 
-                        self.assertEqual(piece.naive_moves, piece.actual_moves)
+                self.assertEqual(
+                    self.knight.actual_moves,
+                    self.knight.naive_moves
+                )
