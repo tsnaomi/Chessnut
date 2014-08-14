6745fce8d9da0c44e4c5f8b48c9df8d3d6f41e2c
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-13 22:20:33 -0700

    Add placeholder TestCases for King and Queen.

---
+++ chessnut_game/tests/pieces/king.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestKing(unittest.TestCase):
+    """Test the King class."""
+    def test_generate_naive_cache(self):
+        """Assert that the naive_moves cache is correctly generated."""
+        pass
