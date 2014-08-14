e409efb01a69df6b8b0adf2dcb8b95a2605738ea
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-13 20:17:36 -0700

    Add list of interesting spaces to tests.pieces.utils.

--- chessnut_game/tests/pieces/utils.py
+++ chessnut_game/tests/pieces/utils.py
@@ -1,4 +1,21 @@
-"""Helper functions for Piece tests."""
+"""Helper functions and constants for Piece tests."""
+
+# Some tests become too complex if every space on the board is tested.
+# Test only a collection of interesting spaces, instead.
+test_spaces = (
+    # Test the spaces in the corners of the board.
+    (0, 0),
+    (0, 7),
+    (7, 0),
+    (7, 7),
+    # Test a space along each side of the board.
+    (0, 4),
+    (4, 0),
+    (7, 4),
+    (4, 7),
+    # Test a space in the middle of the board.
+    (4, 4),
+)
 
 
 def generate_diagonal_spaces(_file, rank):
