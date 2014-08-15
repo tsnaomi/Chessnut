b98ff71cfe8f9d0b0c6811fac0291b2f2d19f456
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-14 22:23:25 -0700

    Update Bishop test suite with decorator imports and actual_cache empty board test.

--- chessnut_game/tests/pieces/bishop.py
+++ chessnut_game/tests/pieces/bishop.py
@@ -1,27 +1,35 @@
 import unittest
-from chessnut_game.game import Black, White
+from ..utils.functions import generate_diagonal_spaces
+from ..utils.decorators import all_players, all_spaces
+from chessnut_game import ChessnutGame
 from chessnut_game.pieces import Bishop
-from .utils import generate_diagonal_spaces
+
+bishop = Bishop()
 
 
 class TestBishop(unittest.TestCase):
     """Test the Bishop class."""
-    def setUp(self):
-        self.pW = Bishop(White, 0, 0)
-        self.pB = Bishop(Black, 0, 7)
-
-    def test_generate_naive_cache(self):
+    @all_spaces(bishop)
+    @all_players(bishop)
+    def test_generate_naive_cache(self, bishop=None):
         """Assert that the naive_moves cache is correctly generated."""
-        for _file, rank in ((f, r) for f in range(8) for r in range(8)):
-            for piece in (self.pW, self.pB):
-                piece.file = _file
-                piece.rank = rank
+        expected_spaces = set(
+            (f, r)
+            for f, r in generate_diagonal_spaces(bishop.file, bishop.rank)
+        )
 
-                expected_spaces = set(
-                    (f, r)
-                    for f, r in generate_diagonal_spaces(_file, rank)
-                )
+        bishop.generate_naive_cache()
 
-                piece.generate_naive_cache()
+        self.assertEqual(bishop.naive_moves, expected_spaces)
 
-                self.assertEqual(piece.naive_moves, expected_spaces)
+    @all_spaces(bishop)
+    @all_players(bishop)
+    def test_generate_actual_cache_empty_board(self, bishop=None):
+        """Assert that the actual_moves cache is identical to the naive_moves
+        cache in the case of an empty board.
+        """
+        game = ChessnutGame()
+        self.assertEqual(
+            bishop.generate_naive_cache(),
+            bishop.generate_actual_cache(game)
+        )
