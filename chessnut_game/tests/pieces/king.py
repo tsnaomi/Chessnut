b555c1ec9bc31dc89d31db693986c415f49a6ada
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-14 21:56:51 -0700

    Add imports and empty board actual_cache test methods to the Queen and King test suites.

--- chessnut_game/tests/pieces/king.py
+++ chessnut_game/tests/pieces/king.py
@@ -1,4 +1,9 @@
 import unittest
+from ..utils.decorators import all_players, all_spaces
+from chessnut_game import ChessnutGame
+from chessnut_game.pieces import King
+
+king = King()
 
 
 class TestKing(unittest.TestCase):
@@ -6,3 +11,15 @@ class TestKing(unittest.TestCase):
     def test_generate_naive_cache(self):
         """Assert that the naive_moves cache is correctly generated."""
         pass
+
+    @all_spaces(king)
+    @all_players(king)
+    def test_generate_actual_cache_empty_board(self, king=None):
+        """Assert that the actual_moves cache is identical to the naive_moves
+        cache in the case of an empty board.
+        """
+        game = ChessnutGame()
+        self.assertEqual(
+            king.generate_naive_cache(),
+            king.generate_actual_cache(game)
+        )
--- chessnut_game/tests/pieces/queen.py
+++ chessnut_game/tests/pieces/queen.py
@@ -1,4 +1,9 @@
 import unittest
+from ..utils.decorators import all_players, all_spaces
+from chessnut_game import ChessnutGame
+from chessnut_game.pieces import Queen
+
+queen = Queen()
 
 
 class TestQueen(unittest.TestCase):
@@ -6,3 +11,15 @@ class TestQueen(unittest.TestCase):
     def test_generate_naive_cache(self):
         """Assert that the naive_moves cache is correctly generated."""
         pass
+
+    @all_spaces(queen)
+    @all_players(queen)
+    def test_generate_actual_cache_empty_board(self, queen=None):
+        """Assert that the actual_moves cache is identical to the naive_moves
+        cache in the case of an empty board.
+        """
+        game = ChessnutGame()
+        self.assertEqual(
+            queen.generate_naive_cache(),
+            queen.generate_actual_cache(game)
+        )
