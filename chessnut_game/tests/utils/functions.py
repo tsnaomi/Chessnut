876836a006ff29b1a7dbbfbcc283023e10ef7a38
Matt Dougherty <thegeekofalltrades@gmail.com>
2014-08-15 19:02:34 -0700

    Add generate_diagonal_spaces to tests.utils.functions.

--- chessnut_game/tests/utils/functions.py
+++ chessnut_game/tests/utils/functions.py
@@ -63,3 +63,17 @@ def block_non_cache_spaces(game, piece=None, blocker=Pawn, player=White):
             game._hard_place_piece(Pawn(player, _file, rank))
 
     return game
+
+
+def generate_diagonal_spaces(_file, rank):
+    """Given a file and a rank, this function generates all of the spaces
+    in the two diagonals that intersect at that space, minus the space
+    itself.
+    """
+    for file_mod, rank_mod in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
+        new_file = _file + file_mod
+        new_rank = rank + rank_mod
+        while 0 <= new_file <= 7 and 0 <= new_rank <= 7:
+            yield (new_file, new_rank)
+            new_file += file_mod
+            new_rank += rank_mod
